cd556115a2082a88bf9beebf74aaa0b9639d8270
Administrator <874520125@qq.com>
2019-12-08 11:54:50 +0800

    电商-用户模块搭建

---
+++ imall-user/src/main/java/com/iport/imall/user/ImallUserApplication.java
@@ -0,0 +1,13 @@
+package com.iport.imall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImallUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImallUserApplication.class, args);
+	}
+
+}
---
+++ imall-user/src/test/java/com/iport/imall/user/ImallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iport.imall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImallUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
